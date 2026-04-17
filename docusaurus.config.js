ec3c1d4628adc25e063924ba8c3609c6175b89e1
babichhunter <hunterbabich@protonmail.com>
2026-04-17 10:38:06 -0500

    Update Sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'ecuConvertSidebar',
+            sidebarId: 'Introduction',
             position: 'left',
             label: 'ECU Conversion',
           },
